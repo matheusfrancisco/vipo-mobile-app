c6618c727378064d776db795042fa363cf7a609d
MayaraSs <may_stein@hotmail.com>
2020-08-12 08:24:02 -0300

    Edit component MatcherParty

--- src/pages/MatcherParty/index.tsx
+++ src/pages/MatcherParty/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
-import { Title, Container } from './styles';
-
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, ScrollView, Platform, Text, Image } from 'react-native';
+import { Title, Container, SearchRole, TextMatcher, ContainerImage, ContainerContent as ContainerContent, TextContainerImage } from './styles';
+import SmoothPicker from "react-native-smooth-picker";
 
 const MatcherParty: React.FC = () => {
+  // const [selected, setSelected] = useState();
+
+  // const handleChange = (index) => {
+  //   setSelected({
+  //     selected: index
+  //   });
+  // };
 
   return  (
     <>
@@ -16,11 +23,42 @@ const MatcherParty: React.FC = () => {
           keyboardShouldPersistTaps="handled"
           contentContainerStyle={{ flex: 1}}
         >
-            <View>
+            <SearchRole>
               <Title>PROCURAR UM ROLÊ </Title>
-            </View>
-          <Container>
-          </Container>
+            </SearchRole>
+            <Container>
+              <TextMatcher>
+                <Text>Em quantas pessoas vocês estão?</Text>
+              </TextMatcher>
+            </Container>
+            {/* <SmoothPicker
+              offsetSelection={40}
+              magnet
+              scrollAnimation
+              data={Array.from({ length: 16 }, (_, i) => i)}
+              onSelected={({ item, index }) => handleChange(index)}
+              renderItem={({ item, index }) => (
+                <Number selected={index === selected}>{item}</Number>
+                )}
+              /> */}
+              <TextMatcher>Quais os planos para hoje?</TextMatcher>
+              <ContainerImage>
+                <ContainerContent>
+                  <Image 
+                      source={require('../../assets/drink.jpg')}  
+                      style={{width: 100, height: 100, borderRadius: 100/ 2}} 
+                  />
+                    <TextContainerImage> Beber</TextContainerImage>
+              </ContainerContent>
+              <ContainerContent>
+                  <Image 
+                      source={require('../../assets/food.jpg')}  
+                      style={{width: 100, height: 100, borderRadius: 100/ 2}} 
+                  />
+                    <TextContainerImage>Comer</TextContainerImage>
+              </ContainerContent>
+            </ContainerImage>
+         
         </ScrollView>
       </KeyboardAvoidingView>
 
--- src/pages/MatcherParty/styles.ts
+++ src/pages/MatcherParty/styles.ts
@@ -2,16 +2,45 @@ import styled from 'styled-components/native';
 import { Platform } from 'react-native';
 
 export const Container = styled.View`
-  flex:1;
-  align-items: center;
-  justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
-  `;
+  padding: 0 0px ${Platform.OS === 'android' ? 20 : 40}px;
+`;
 
+export const ContainerImage = styled.View`
+  padding: 0 0px ${Platform.OS === 'android' ? 20 : 40}px;
+  margin-left: 10px;
+  flex-direction: row;
+`;
+
+export const ContainerContent= styled.View`
+  flex-direction:column;
+  margin-right: 20px;
+`;
+
+export const TextContainerImage = styled.Text`
+  color: #FFF;
+  text-align: center;
+  margin-top: 2px;
+`;
+
+
+export const SearchRole = styled.View`
+  background-color: #871F9A;
+
+`;
+
+
+export const TextMatcher = styled.Text`
+  font-size: 15px;
+  color: #fff;
+  font-family: 'RobotoSlab-Medium';
+  margin: 20px;
+  text-align: left;
+`;
 
 export const Title = styled.Text`
   font-size: 25px;
-  color: #871F9A;
+  color: #fff;
   font-family: 'RobotoSlab-Medium';
-  margin: 14px 0 54px;
+  margin: 30px;
+  text-align: center;
 `;
