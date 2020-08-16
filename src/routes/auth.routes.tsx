21acbbc642170345da6d938f51aa25716ad31f89
MayaraSs <may_stein@hotmail.com>
2020-08-16 20:41:03 -0300

    Edit name route

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import MatcherParty from '../pages/MatcherParty';
+import LikeMusic from '../pages/LikeMusic';
+import TasteFood from '../pages/TasteFood';
+import TasteDrink from '../pages/TasteDrink';
 
 const Auth = createStackNavigator();
 
@@ -14,6 +18,10 @@ const AuthRoutes: React.FC = () => (
     >
       <Auth.Screen name="SignIn" component={SignIn} />
       <Auth.Screen name="SignUp" component={SignUp} />
+      <Auth.Screen name="MatcherParty" component={MatcherParty} />
+      <Auth.Screen name="LikeMusic" component={LikeMusic} />
+      <Auth.Screen name="TasteFood" component={TasteFood} />
+      <Auth.Screen name="TasteDrink" component={TasteDrink} />
     </Auth.Navigator>
 );
 
