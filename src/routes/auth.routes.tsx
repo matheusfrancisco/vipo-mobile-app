551a5d7a007d58c3b9a9266ea0ffb71cee2b5d4c
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-08-16 20:11:50 -0300

    fix conflict

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-<<<<<<< HEAD
-import MatcherParty from '../pages/MatcherParty';
-import LikeMusical from '../pages/LikeMusic';
-import TasteFood from '../pages/TasteFood';
-import MatchParty from '../pages/Match';
-=======
->>>>>>> Edit navigate to add all fluxo
 
 const Auth = createStackNavigator();
 
