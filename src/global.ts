bef11a8873b3d88526f05085feba4bed1952dcce
MayaraSs <may_stein@hotmail.com>
2020-10-24 14:59:29 -0300

    Add components text

--- src/global.ts
+++ src/global.ts
@@ -15,17 +15,29 @@ export const TextH3 = styled.Text`
 
 export const TextH4 = styled.Text`
   font-size: 12px;
-  color: #9283BF;
-  font-family: "RobotoSlab-Medium";
+  color: #9283bf;
+  font-family: 'RobotoSlab-Medium';
 `;
 export const TextMin = styled.Text`
   font-size: 12px;
   color: #000;
-  font-family: "RobotoSlab-Medium";
+  font-family: 'RobotoSlab-Medium';
 `;
 export const TextDescription = styled.Text`
   color: #000000;
   font-family: 'RobotoSlab-Medium';
   text-align: center;
   font-size: 16px;
+`;
+export const TextH1 = styled.Text`
+  color: #fff;
+  font-family: 'RobotoSlab-Medium';
+  text-align: center;
+  font-size: 25px;
+`;
+export const TextMinAsker = styled.Text`
+  font-size: 15px;
+  color: #000;
+  font-family: 'RobotoSlab-Medium';
+  align-self: center;
 `;
