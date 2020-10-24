c0b4fa6f9db1ad0b88b48c65a90ea2ab752b48d1
mayarastein <35180942+MayaraSs@users.noreply.github.com>
2020-10-24 15:31:54 -0300

    Refactor profileMusic (#26)
    * Refactor profileMusic
    
    * Edit component ProfileMusic

---
+++ src/pages/ProfileMusic/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+`;
+
+export const HeaderText = styled.View`
+  align-items: center;
+  padding: 20px;
+`;
+
+export const ContainerImage = styled.View`
+  flex-direction: row;
+  padding: 15px;
+`;
+
+export const ContainerContent = styled.View`
+  flex-direction: column;
+  margin: 0px 15px;
+`;
+
+export const Next = styled.View`
+  position: absolute;
+  width: 4px;
+  height: 4px;
+  left: 46px;
+  top: 716px;
+  background: #c1bccc;
+  border-radius: 1px;
+  transform: rotate(-180deg);
+`;
+
+export const Line = styled.Text`
+  border-bottom-width: 1px;
+  border-bottom-color: #9283bf;
+  height: 20px;
+  width: 80%;
+  position: relative;
+  left: 10px;
+  bottom: 8px;
+`;
