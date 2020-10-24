76c4d18abba63c2a08068104f77b551e6fa42c83
mayarastein <35180942+MayaraSs@users.noreply.github.com>
2020-10-24 15:17:01 -0300

    Add component Header (#24)

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Container, IconBorder, TextNext} from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {TextH1} from '../../global';
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <IconBorder>
+          <Icon
+            name="chevron-left"
+            color="#fff"
+            onPress={() => console.log('Foi')}
+          />
+        </IconBorder>
+        <TextNext>
+          <TextH1 onPress={() => console.log('Foi')}>Próxima</TextH1>
+        </TextNext>
+      </Container>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  background: #9283bf;
+  width: 100%;
+  height: 70px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const IconBorder = styled.View`
+  margin: 20px 0px 25px 20px;
+  border-width: 6px;
+  border-color: #a396cb;
+  background: #a396cb;
+`;
+
+export const TextNext = styled.View`
+  margin: 12px 20px 0px 0px;
+`;
