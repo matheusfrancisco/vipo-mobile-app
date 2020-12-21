40b975df449554e1162c62826cb8cd4408dde554
MayaraSs <may_stein@hotmail.com>
2020-12-21 16:59:45 -0300

    Edit routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -27,7 +27,7 @@ const AuthRoutes: React.FC = () => (
       headerShown: false,
       cardStyle: {backgroundColor: '#F8FBFA'},
     }}
-    initialRouteName="LandingPage">
+    initialRouteName="PickerMatcherParty">
     <Auth.Screen
       name="RegistrationCompleted"
       component={RegistrationCompleted}
