d57a87f2d238bf36bcce7aaaba54d21eca6ee900
MayaraSs <may_stein@hotmail.com>
2021-02-17 12:58:47 -0300

    add control is gone to the role

--- src/pages/Feedback/styles.ts
+++ src/pages/Feedback/styles.ts
@@ -7,7 +7,7 @@ export const Header = styled.Text`
 `;
 
 export const FeedbackMax = styled.Text`
-  margin: 20px;
+  margin: 12px;
   color: #470a68;
 `;
 
@@ -19,18 +19,23 @@ export const AlignCenter = styled.View`
   align-self: center;
 `;
 
+
 export const Container = styled.View`
   margin: 10px;
 `;
 export const ColorText = styled.Text`
   color: #470a68;
 `;
+interface Props {
+  select?: boolean;
+}
 export const SelectFeedback = styled(RectButton)<Props>`
-  ${(props) =>
-    props.select &&
-    css`
-      opacity: 0.3;
-    `}
+  ${(props: any) =>
+      props.select &&
+      css`
+        opacity: 0.3;
+        
+      `}
 `;
 
 export const ImageItem = styled.Image`
@@ -43,21 +48,31 @@ export const Image = styled.View`
   justify-content: space-around;
 `;
 
-interface Props {
-  select?: boolean;
-}
 export const Types = styled.Text<Props>`
-  margin: 5px;
+  margin: 4px;
   border: 2px solid #000;
-  width: 90px;
+  width: 100px;
   height: 20px;
   position: relative;
   border-radius: 5px;
   text-align: center;
-  ${(props) =>
+  ${(props: any) =>
     props.select &&
     css`
       background-color: #9283bf;
       font-weight: bold;
     `}
 `;
+
+export const Genre = styled.View`
+  margin-top: 15px;
+  border: 1px solid #dadfe2;
+  border-radius: 10px;
+`
+
+export const GenreText = styled.Text`
+  font-size: 15px;
+  font-family: 'RobotoSlab-Medium';
+  margin: 5px 0px 10px 15px;
+`
+
