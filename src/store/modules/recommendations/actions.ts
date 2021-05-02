ed993ea1c453ee1ad75e3076c2fcf168df5137a7
Raul <raul.hmetal@gmail.com>
2021-05-02 11:47:40 -0300

    Edit: Deleted store folder

--- src/store/modules/recommendations/actions.ts
+++
@@ -1,9 +0,0 @@
-import { action } from 'typesafe-actions';
-import { AnswersTypes } from './types';
-
-export const answeredRecommendtionsRequest = (answers: any) => ({
-  type: action(AnswersTypes.ANSWER_REQUEST, { answers }),
-});
-export const answeredRecommendtionsSuccess = (answers: any) => ({
-  type: action(AnswersTypes.ANSWER_SUCCESS, { answers }),
-});
--- src/store/modules/rootSaga.ts
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import profileSaga from './profile/saga';
-import answerSaga from './recommendations/saga';
-
-export const rootSaga = function* rootSaga() {
-  yield all([profileSaga(), answerSaga()]);
-};
