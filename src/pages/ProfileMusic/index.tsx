befe6678a8dce885928d52b6001c455c99b10652
Raul <raul.hmetal@gmail.com>
2021-05-16 11:30:43 -0300

    Edit: Refactoring ProfileMusic

--- src/pages/ProfileMusic/index.tsx
+++ src/pages/ProfileMusic/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
+import React, { useMemo } from 'react';
+import { ImageURISource } from 'react-native';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 
 import { Container, HeaderText } from './styles';
 
 import { TextMinAsker } from '../../global';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import Line from '../../components/Line';
-import BodyProfileMusic from './Body';
+import RecommendationsList from '../../components/RecommendationsList';
+import { Formik } from 'formik';
 
 interface IProfileDrink {
   foods: Array<string>;
@@ -22,6 +22,42 @@ type ParamList = {
   };
 };
 
+interface IMusicOption {
+  name: string;
+  link: ImageURISource;
+}
+
+const musicOptions: IMusicOption[] = [
+  {
+    link: require('../../assets/style-music/eletronica.jpeg'),
+    name: 'Eletronica',
+  },
+  { link: require('../../assets/style-music/funk.jpg'), name: 'Funk' },
+  { link: require('../../assets/style-music/pagode.jpg'), name: 'Pagode' },
+  { link: require('../../assets/style-music/rock.jpg'), name: 'Rock' },
+  { link: require('../../assets/style-music/forró.jpg'), name: 'Forró' },
+  { link: require('../../assets/style-music/jazz.jpeg'), name: 'Jazz' },
+  { link: require('../../assets/style-music/pop.jpg'), name: 'Pop' },
+  { link: require('../../assets/style-music/reggae.jpeg'), name: 'Reggae' },
+  { link: require('../../assets/style-music/samba.jpg'), name: 'Samba' },
+  {
+    link: require('../../assets/style-music/sertanejo.jpg'),
+    name: 'Sertanejo',
+  },
+  { link: require('../../assets/style-music/hiphop.jpg'), name: 'Hip Hop' },
+  { link: require('../../assets/style-music/mpb.jpg'), name: 'MPB' },
+  { link: require('../../assets/style-music/indie.jpg'), name: 'Indie' },
+  {
+    link: require('../../assets/style-music/reggaeton.jpg'),
+    name: 'Reggaeton',
+  },
+  { link: require('../../assets/style-music/rap.jpg'), name: 'Rap' },
+  {
+    link: require('../../assets/style-music/rockalternativo.jpeg'),
+    name: 'Rock Alternativo',
+  },
+];
+
 type ProfileMusicParams = RouteProp<ParamList, 'ProfileDrink'>;
 
 const ProfileMusic: React.FC = () => {
@@ -30,28 +66,44 @@ const ProfileMusic: React.FC = () => {
     params: { profileInformations },
   } = useRoute<ProfileMusicParams>();
 
-  console.log(profileInformations);
+  const initialValues = useMemo(
+    () => ({
+      musicals: musicOptions.reduce(
+        (acc, option) => ({
+          ...acc,
+          [option.name]: profileInformations.musicals.includes(option.name),
+        }),
+        {},
+      ),
+    }),
+    [profileInformations.musicals],
+  );
 
-  const undoPage = () => {
-    navigation.goBack();
+  const handleSubmit = (values: typeof initialValues) => {
+    const musicals = Object.entries(values.musicals)
+      .map(([name, value]) => (value ? name : undefined))
+      .filter((value) => value);
+
+    const newProfileInformations = { ...profileInformations, musicals };
+
+    console.log(newProfileInformations);
+
+    // navigation.navigate('Profile');
   };
+
   return (
-    <>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        enabled>
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          contentContainerStyle={{ flex: 1 }}>
-          <Container>
-            <Header
-              text="Salvar"
-              onPress={() => {
-                navigation.navigate('Profile');
-              }}
-              onPressBack={undoPage}
-            />
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {({ submitForm }) => (
+        <>
+          <Header
+            text="Salvar"
+            onPress={() => {
+              submitForm();
+            }}
+            onPressBack={navigation.goBack}
+          />
+
+          <Container keyboardShouldPersistTaps="handled">
             <HeaderText>
               <TextMinAsker>
                 Nós ajude a indicar os lugares de acordo com
@@ -60,12 +112,14 @@ const ProfileMusic: React.FC = () => {
             </HeaderText>
             <Line />
 
-            <BodyProfileMusic />
+            <RecommendationsList
+              recommendations={musicOptions}
+              fieldName="musicals"
+            />
           </Container>
-        </ScrollView>
-        <Footer />
-      </KeyboardAvoidingView>
-    </>
+        </>
+      )}
+    </Formik>
   );
 };
 
--- src/pages/ProfileMusic/styles.ts
+++ src/pages/ProfileMusic/styles.ts
@@ -1,35 +1,10 @@
-import styled, { css } from 'styled-components/native';
-import { RectButton } from 'react-native-gesture-handler';
-
-export const Container = styled.View`
-  align-items: center;
-`;
+import styled from 'styled-components/native';
 
 export const HeaderText = styled.View`
   align-items: center;
   padding: 20px;
 `;
 
-export const Row = styled.View`
-  flex-direction: row;
-  padding: 15px;
-`;
-
-export const AlignContent = styled.View`
-  flex-direction: column;
-  margin: 0px 15px;
-`;
-
-interface Props {
-  select?: boolean;
-}
-export const ContainerText = styled(RectButton)<Props>`
-  ${(props) =>
-    props.select &&
-    css`
-      opacity: 0.3;
-    `}
-`;
-export const TextMusic = styled.Text`
-  color: #9283bf;
+export const Container = styled.ScrollView`
+  height: 100%;
 `;
