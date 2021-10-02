c8d63598decfa6eb7d5069c9d4eaf37ef1414d2f
Matheus Francisco <matheusmachadoufsc@gmail.com>
2021-10-02 13:08:50 -0300

    add splash screen to wait app load (#85)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import AppUser from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e28" />
-    <AppUser>
-      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-        <Routes />
-      </View>
-    </AppUser>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e28" />
+      <AppUser>
+        <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+          <Routes />
+        </View>
+      </AppUser>
+    </NavigationContainer>
+  );
+};
 
 export default App;
