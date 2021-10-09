f4242257f5c75eca6fa5677f5bf5f0cb122d015b
Raul <raul.hmetal@gmail.com>
2021-10-09 11:24:53 -0300

    Add: theme config files

---
+++ src/config/theme/index.ts
@@ -0,0 +1,11 @@
+import layout from './layout';
+import palette from './palette';
+import typography from './typography';
+
+const theme = {
+  layout,
+  palette,
+  typography,
+};
+
+export default theme;
