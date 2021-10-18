f85e925db8577a5dacbf6880e86a1393fe774f20
Raul <raul.hmetal@gmail.com>
2021-10-18 10:21:32 -0300

    Add: import rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     extends: [
         '@react-native-community',
         'eslint:recommended',
+        "plugin:import/recommended",
         "plugin:react/recommended",
         'plugin:@typescript-eslint/recommended',
         "plugin:prettier/recommended",
@@ -17,5 +18,32 @@ module.exports = {
         "no-shadow": 0,
         "prettier/prettier": ['error', {endOfLine: 'auto'}],
         "@typescript-eslint/no-explicit-any": 0,
+        'import/no-unresolved': 0,
+        'import/named': 0,
+        'import/namespace': 0,
+        "import/order": [
+          "error",
+          {
+            "groups": ["builtin", "external", "internal"],
+            "pathGroups": [
+              {
+                "pattern": "react+(|-native)",
+                "group": "external",
+                "position": "before"
+              },
+              {
+                "pattern": "@/**",
+                "group": "internal",
+                "position": "before"
+              }
+            ],
+            "pathGroupsExcludedImportTypes": ["react+(|-native)", "@/**"],
+            "newlines-between": "always",
+            "alphabetize": {
+              "order": "asc",
+              "caseInsensitive": true
+            }
+          }
+        ],
     }
 };
