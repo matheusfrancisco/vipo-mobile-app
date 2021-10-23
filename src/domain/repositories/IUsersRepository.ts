c6c39e519a0fb4a587e8d2ee04adc9e66310608f
Raul <raul.hmetal@gmail.com>
2021-10-23 12:25:32 -0300

    edit: patch profile on users repository

--- src/domain/repositories/IUsersRepository.ts
+++ src/domain/repositories/IUsersRepository.ts
@@ -1,5 +1,7 @@
+import IProfile from '@/domain/entities/IProfile';
 import { IUserProfile } from '@/domain/entities/IUser';
 
 export interface IUsersRepository {
   getProfile(userId: string): Promise<IUserProfile>;
+  updateProfile(userId: string, profile: IProfile): Promise<void>;
 }
