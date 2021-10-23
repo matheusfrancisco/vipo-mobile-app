f382e934b4be8fef98f03525f9bb0d882316ae96
Raul <raul.hmetal@gmail.com>
2021-10-23 12:35:32 -0300

    edit: updating and creating user on users repository

--- src/domain/repositories/IUsersRepository.ts
+++ src/domain/repositories/IUsersRepository.ts
@@ -1,7 +1,24 @@
 import IProfile from '@/domain/entities/IProfile';
-import { IUserProfile } from '@/domain/entities/IUser';
+import IUser, { IUserProfile } from '@/domain/entities/IUser';
+
+interface ICreateUser {
+  name: string;
+  email: string;
+  password: string;
+  gender: string;
+  birthDate: string;
+}
+
+interface IUpdateUser {
+  name: string;
+  lastName: string;
+  address: string;
+}
 
 export interface IUsersRepository {
+  create(user: ICreateUser): Promise<void>;
+  update(user: IUpdateUser): Promise<IUser>;
+
   getProfile(userId: string): Promise<IUserProfile>;
   updateProfile(userId: string, profile: IProfile): Promise<void>;
 }
