1cdd5a188a7e7b6a6231a1a0a12d6565d730e40a
Raul <raul.hmetal@gmail.com>
2021-10-29 09:45:03 -0300

    add: jest initial configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,23 @@ module.exports = {
         "plugin:react/recommended",
         'plugin:@typescript-eslint/recommended',
         "plugin:prettier/recommended",
+        "plugin:jest/style",
+		    "plugin:testing-library/react",
     ],
     plugins: [
         "react",
         "@typescript-eslint",
         "prettier",
-        "react-hooks"
+        "react-hooks",
+        "jest",
+        "testing-library"
     ],
     rules: {
         "no-shadow": 0,
         "prettier/prettier": ['error', {endOfLine: 'auto'}],
         "@typescript-eslint/no-explicit-any": 0,
+        "testing-library/prefer-screen-queries": "off",
+        "testing-library/render-result-naming-convention": "off",
         'import/no-unresolved': 0,
         'import/named': 0,
         'import/namespace': 0,
